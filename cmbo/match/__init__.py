bd3e2576e2b53eb7b27f989badf3190c5d461939
Richard-Sti <richard.stiskalek@protonmail.com>
2025-11-19 23:09:40 +0100

    Update matching

--- cmbo/match/__init__.py
+++ cmbo/match/__init__.py
@@ -16,5 +16,4 @@ from .assignments import (  # noqa: F401
     match_planck_catalog_to_associations,
     match_mcxc_catalog_to_associations,
     match_erass_catalog_to_associations,
-    match_planck_and_xray_joint,
 )
